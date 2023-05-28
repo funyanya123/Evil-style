515cab12170ee8c18f202a5c4511bb6effe51d35
k.k <106207231+funyanya123@users.noreply.github.com>
2023-05-28 17:17:40 +0900

    Update cartoonify.cpp

--- Evil style/include/cartoonify.cpp
+++ Evil style/include/cartoonify.cpp
@@ -20,3 +20,19 @@ void Cartoonifycamera(const cv::Mat& input, cv::Mat& output) {
     cv::bitwise_not(output, output);
     cv::add(output, cv::Scalar(0, 0, 100), output);
 }
+
+void Comicifycamera(const cv::Mat& input, cv::Mat& output) {
+    // Convert the image to grayscale
+    cv::Mat gray;
+    cv::cvtColor(input, gray, cv::COLOR_BGR2GRAY);
+
+    // Apply median blur to reduce noise
+    cv::medianBlur(gray, gray, 7);
+
+    // Generate a binary image using adaptive thresholding
+    cv::Mat edges;
+    cv::adaptiveThreshold(gray, edges, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 9, 2);
+
+    // Convert the grayscale image to BGR format
+    cv::cvtColor(edges, output, cv::COLOR_GRAY2BGR);
+}
